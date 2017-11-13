e4b553d3d2b1015f79d7bc75cd7a87b9b6cc15c7
Josh <joshrideout88@gmail.com>
2017-11-13 08:47:45 +0700

    commit

---
+++ ios/IJKMediaDemo/IJKMediaDemo/IJKMultipleViewController.h
@@ -0,0 +1,13 @@
+//
+//  IJKMultipleViewController.h
+//  IJKMediaDemo
+//
+//  Created by Josh Rideout on 10/15/17.
+//  Copyright © 2017 bilibili. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IJKMultipleViewController : UIViewController
+
+@end
---
+++ ios/IJKMediaDemo/IJKMediaDemo/IJKMultipleViewController.m
@@ -0,0 +1,37 @@
+//
+//  IJKMultipleViewController.m
+//  IJKMediaDemo
+//
+//  Created by Josh Rideout on 10/15/17.
+//  Copyright © 2017 bilibili. All rights reserved.
+//
+
+#import "IJKMultipleViewController.h"
+
+@interface IJKMultipleViewController ()
+
+@end
+
+@implementation IJKMultipleViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
